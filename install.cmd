e15fd1f089cf6205a952fcb6ac8b951c43c78894
Marcel Gerber (BNC) <92036000+bncmge@users.noreply.github.com>
2021-11-11 10:27:29 +0100

    add pluginver variable

--- install.cmd
+++ install.cmd
@@ -1,5 +1,6 @@
 @echo off
 
+set pluginver=v2.0.0
 set npp_x86=%ProgramFiles(x86)%\Notepad++
 set npp_x64=%ProgramFiles%\Notepad++
 
@@ -8,11 +9,11 @@ if "%ERRORLEVEL%" == "0" taskkill /im notepad++.exe
 
 :: set 32 bit version
 if exist "%npp_x86%" set plugindir=%npp_x86%\plugins\ComparePlugin
-if exist "%npp_x86%" set pluginsrc=ComparePlugin_v2.0.0_x86
+if exist "%npp_x86%" set pluginsrc=ComparePlugin_%pluginver%_x86
 
 :: set 64 bit version
 if exist "%npp_x64%" set plugindir=%npp_x64%\plugins\ComparePlugin
-if exist "%npp_x64%" set pluginsrc=ComparePlugin_v2.0.0_X64
+if exist "%npp_x64%" set pluginsrc=ComparePlugin_%pluginver%_X64
 
 :: install plugin
 echo Install plugin '%pluginsrc%' to '%plugindir%'
